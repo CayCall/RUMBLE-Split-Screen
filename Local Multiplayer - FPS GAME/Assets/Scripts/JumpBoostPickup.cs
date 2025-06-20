43f63b31d98a2992217393f197f8cc0f3eca6612
Amro <2592997@students.wits.ac.za>
2025-06-20 02:33:19 +0200

    Overlay

--- Local Multiplayer - FPS GAME/Assets/Scripts/JumpBoostPickup.cs
+++ Local Multiplayer - FPS GAME/Assets/Scripts/JumpBoostPickup.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class JumpBoostPickup : MonoBehaviour
 {
     [Header("Jump Boost Settings")]
     public float jumpBoostAmount = 5f;
     public float boostDuration = 5f;
-    public GameObject Player1Overlay;
-    public GameObject Player2Overlay;
+    public RawImage Player1Overlay;
+    public RawImage Player2Overlay;
+
+    void Start()
+    {
+        Player1Overlay = GameObject.FindGameObjectWithTag("Player1Overlay").GetComponent<RawImage>();
+        Player2Overlay = GameObject.FindGameObjectWithTag("Player2Overlay").GetComponent<RawImage>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -43,6 +51,12 @@ public class JumpBoostPickup : MonoBehaviour
         }
     }
 
+    void SetOpacity(RawImage rawImage, float alphaValue)
+    {
+        Color color = rawImage.color;
+        color.a = alphaValue / 255f;  // Convert alpha from 0-255 range to 0-1
+        rawImage.color = color;
+    }
 
     private System.Collections.IEnumerator ApplyJumpBoost(MonoBehaviour playerScript)
     {
@@ -50,18 +64,22 @@ public class JumpBoostPickup : MonoBehaviour
 
         if (playerScript is PlayerController player1)
         {
+            SetOpacity(Player1Overlay, 187);
             originalJump = player1.GetJumpForce();
             player1.SetJumpForce(originalJump + jumpBoostAmount);
             yield return new WaitForSeconds(boostDuration);
             player1.SetJumpForce(originalJump);
+            SetOpacity(Player1Overlay, 0);
         }
         else if (playerScript is Player2Controller player2)
         {
+            SetOpacity(Player2Overlay, 187);
             originalJump = player2.GetJumpForce();
             player2.SetJumpForce(originalJump + jumpBoostAmount);
             yield return new WaitForSeconds(boostDuration);
             player2.SetJumpForce(originalJump);
             Debug.Log("Player Two Original Jump" + originalJump);
+            SetOpacity(Player1Overlay, 0);
         }
 
         Destroy(gameObject); // Optional: delete the pickup
--- Local Multiplayer - FPS GAME/Assets/Scripts/SpeedBoostPickup.cs
+++ Local Multiplayer - FPS GAME/Assets/Scripts/SpeedBoostPickup.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeedBoostPickup : MonoBehaviour
 {
     [Header("Speed Boost Settings")]
     public float speedBoostAmount = 3f;
     public float boostDuration = 5f;
+    public RawImage Player1Overlay;
+    public RawImage Player2Overlay;
+
+    void Start()
+    {
+        Player1Overlay = GameObject.FindGameObjectWithTag("Player1Overlay").GetComponent<RawImage>();   
+        Player2Overlay = GameObject.FindGameObjectWithTag("Player2Overlay").GetComponent<RawImage>();   
+    }
+
+    void SetOpacity(RawImage rawImage, float alphaValue)
+    {
+        Color color = rawImage.color;
+        color.a = alphaValue / 255f;  // Convert alpha from 0-255 range to 0-1
+        rawImage.color = color;
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -47,17 +64,21 @@ public class SpeedBoostPickup : MonoBehaviour
         // Try to call the right Get/Set methods based on type
         if (playerScript is PlayerController player1)
         {
+            SetOpacity(Player1Overlay, 187);
             originalSpeed = player1.GetMoveSpeed();
             player1.SetMoveSpeed(originalSpeed + speedBoostAmount);
             yield return new WaitForSeconds(boostDuration);
             player1.SetMoveSpeed(originalSpeed);
+            SetOpacity(Player1Overlay, 0);
         }
         else if (playerScript is Player2Controller player2)
         {
+            SetOpacity(Player2Overlay, 0);
             originalSpeed = player2.GetMoveSpeed();
             player2.SetMoveSpeed(originalSpeed + speedBoostAmount);
             yield return new WaitForSeconds(boostDuration);
             player2.SetMoveSpeed(originalSpeed);
+            SetOpacity(Player2Overlay, 0);
         }
 
         Destroy(gameObject); // Optional: remove pickup completely
